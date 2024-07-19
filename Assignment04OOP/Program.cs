7684024ea333aa5ab01cf2a97e2a687c113e494b
Mohamed Osama Ali <ana20332183@gmail.com>
2024-07-19 20:12:57 +0300

    Interface

--- Assignment04OOP/Program.cs
+++ Assignment04OOP/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Assignment04OOP
+﻿using Assignment04OOP.Interfaces;
+
+namespace Assignment04OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //IMyType myType;
+            //MyType myType1 = new MyType();
+            //myType1 = new MyType();
             
+            IMyType myType = new MyType();
+            myType.Id = 3;
+            myType.Print();
+            myType.MyFun(10);
         }
     }
 }
