8f107b48b527733c1473dc9054ff0239313df328
Mohamed Osama Ali <ana20332183@gmail.com>
2024-07-19 21:52:53 +0300

    Ex01

--- Assignment04OOP/Program.cs
+++ Assignment04OOP/Program.cs
@@ -4,16 +4,38 @@ namespace Assignment04OOP
 {
     internal class Program
     {
+        public static void Print10NumbersFromSeries(ISeries series)
+        {
+            if (series is not null)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write($" {series.Current} ");
+                    series.GetNext();
+                }
+                series.Reset();
+            }
+        }
         static void Main(string[] args)
         {
+            #region Interface
             //IMyType myType;
             //MyType myType1 = new MyType();
             //myType1 = new MyType();
-            
-            IMyType myType = new MyType();
-            myType.Id = 3;
-            myType.Print();
-            myType.MyFun(10);
+
+            //IMyType myType = new MyType();
+            //myType.Id = 3;
+            //myType.Print();
+            //myType.MyFun(10); 
+            #endregion
+            #region Ex01
+            //SeriesByTwo seriesByTwo = new SeriesByTwo();
+            //Print10NumbersFromSeries(seriesByTwo);
+            //Console.WriteLine("\n ==================");
+            //SeriesByThree seriesByThree = new SeriesByThree();
+            //Print10NumbersFromSeries(seriesByThree);
+            #endregion
+
         }
     }
 }
